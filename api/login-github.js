c002ce684fe5e313054167cd13facb4936c7a8f6
mihai10075 <mihai2staicu@gmail.com>
2026-03-03 01:07:53 +0200

    Add files via upload

--- api/login-github.js
+++ api/login-github.js
@@ -1,7 +1,9 @@
 // api/login-github.js
 module.exports = async function handler(req, res) {
   const clientId = process.env.GITHUB_CLIENT_ID;
-  const redirectUri = "https://demo-website-6ub1o4xo9-mihai10075s-projects.vercel.app/api/github-callback";
+
+  // use your stable domain here
+  const redirectUri = "https://demo-website-one-ashy.vercel.app/api/github-callback";
   const state = "mihai-state"; // TODO: for real security, generate random per session
 
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${encodeURIComponent(
